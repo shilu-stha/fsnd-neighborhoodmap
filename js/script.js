964ff938a5bee4f483be6911aaa00e36e476ae51
Shilu Shrestha <sth.shilu@gmail.com>
2018-05-16 12:54:11 +0545

    Adding view model and octopus

--- js/script.js
+++ js/script.js
@@ -1,59 +1,108 @@
-var cats = [
-    {id:1, name:"Hissi", picture:"css/hissi_biralu.jpg", point:0},
-    {id:2, name:"Lajalu", picture:"css/lajalu_biralu.jpg", point:0},
-    {id:3, name:"Alchi", picture:"css/alchi_biralu.jpg", point:0},
-    {id:4, name:"Chakkapareko", picture:"css/chakkapareko_biralu.jpg", point:0},
-    {id:5, name:"Motee", picture:"css/motee_biralu.jpg", point:0},
-    {id:6, name:"Sutuwa", picture:"css/sutuwa_biralu.jpg", point:0}
-];
+var model = {
 
+	currentCat: null,
 
-function catClick() {
-
-    $(document).ready(function(){
-
-        var $body = $('body');   
-        var $catListContainerElem = $('#cat_list_container'); 
-        var $catListElem = $('#cats_list');
-        
-        for (var i = 0; i < cats.length; i++) {
-            var cat = cats[i];
-            $catListElem.append('<li class="cat_item">'+
-                '<p>' + cat.name + '</p>'+
-                '<img class="list_item" id="cat_'+ cat.id +
-                '" src="'+ cat.picture +'">'+
-                '</li>');
-
-            var catItem = document.getElementById('cat_'+cat.id);
-
-            catItem.addEventListener('click',(function(catCopy){
-                    return function() {
-                        updateSelectedContainer(catCopy);
-                };
-            })(cat));
-        };
-        updateSelectedContainer(cats[0]);
-    });
+	cats: [
+	    {id:1, name:"Hissi", imageSource:"css/hissi_biralu.jpg", clickCount:0},
+	    {id:2, name:"Lajalu", imageSource:"css/lajalu_biralu.jpg", clickCount:0},
+	    {id:3, name:"Alchi", imageSource:"css/alchi_biralu.jpg", clickCount:0},
+	    {id:4, name:"Chakkapareko", imageSource:"css/chakkapareko_biralu.jpg", clickCount:0},
+	    {id:5, name:"Motee", imageSource:"css/motee_biralu.jpg", clickCount:0},
+	    {id:6, name:"Sutuwa", imageSource:"css/sutuwa_biralu.jpg", clickCount:0}
+	]
 };
 
-function updateSelectedContainer(selectedItem) {
-   
-    $(document).ready(function(){
-         
-        var catNameElem = document.getElementById('selected_item_header');    
-        var catPointElem = document.getElementById('selected_item_point');
-        var catImageElem = document.getElementById('selected_item');
-        
-        catNameElem.innerText = selectedItem.name;
-        catImageElem.src = selectedItem.picture;
-        catPointElem.innerText = "Points: "+selectedItem.point;
+var octopus = {
 
-        catImageElem.addEventListener('click',function(){
-            //the element has been clicked... do stuff here
-            selectedItem.point = selectedItem.point + 1;
-            catPointElem.innerText = "Points: "+ selectedItem.point;
-        });
-    });
+	getAllCats: function(){
+		return model.cats;
+	},
+
+	getCurrentCat: function(catID) {
+		return model.currentCat;
+	},
+
+	setCurrentCat: function(cat) {
+		model.currentCat = cat;
+	},
+
+	incrementClickCount: function(){
+		model.currentCat.clickCount ++;
+		catView.render();
+	},
+
+	init: function() {
+		model.currentCat = model.cats[0];
+
+		catListView.init();
+		catView.init();
+	}
+
 };
 
-catClick();
+var catView = {
+
+	init: function() {
+		$(document).ready(function(){
+			this.catNameElem = document.getElementById('current_item_header');    
+	        this.catclickCountElem = document.getElementById('current_item_clickCount');
+	        this.catImageElem = document.getElementById('current_item');
+	       
+	       	catImageElem.addEventListener('click',function(){
+	            octopus.incrementClickCount();
+	        });
+       	});
+
+        this.render();
+	},
+
+	render: function() {
+		var currentCat = octopus.getCurrentCat();
+
+		this.catNameElem.textContent = currentCat.name;
+    	this.catImageElem.src = currentCat.imageSource;
+    	this.catclickCountElem.textContent = "ClickCounts: "+currentCat.clickCount;
+	}
+
+};
+
+var catListView = {
+
+	init: function() {
+		this.catListElem = document.getElementById('cat_list');
+		
+		this.render();
+	},
+
+	render: function() {
+		var catList = octopus.getAllCats();
+
+		for (var i = 0; i < catList.length; i++) {
+        	var cat = catList[i];
+
+        	var listElem = document.createElement('li');
+        	listElem.class = 'cat_item';
+        	listElem.textContent = cat.name;
+
+        	var imgElement = document.createElement('img');
+        	imgElement.src = cat.imageSource;
+        	imgElement.class = 'img_item';
+        	// imgElement.id = 'cat_'+cat.id;
+
+        	listElem.appendChild(imgElement);
+
+        	// var catItem = document.getElementById('cat_'+cat.id);
+
+        	listElem.addEventListener('click',(function(cat){
+                return function() {
+                    octopus.setCurrentCat(cat);
+                    catView.render();
+            	};
+        	})(cat));
+
+        	this.catListElem.appendChild(listElem);
+    	};
+	}
+};
+
+octopus.init();
